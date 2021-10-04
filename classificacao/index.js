cd64d98fadd4b6cda55285e47e3e5b0207a98a20
Rodrigo Bossini <professorbossini@gmail.com>
2021-10-04 11:33:48 -0300

    Fizemos o Bloco de Código 4.3.26. Retomar fazendo o Bloco de Codigo 4.3.27

---
+++ classificacao/index.js
@@ -0,0 +1,26 @@
+const { default: axios } = require('axios')
+const express = require ('express')
+const app = express()
+app.use (express.json())
+const palavraChave = 'importante'
+
+const funcoes = {
+    ObservacaoCriada: (observacao) => {
+        observacao.status = 
+            observacao.texto.includes(palavraChave) ? 'Importante' : "Comum"
+        // if (observacao.texto.includes(palavraChave))
+        //     observacao.status = "Importante"
+        // else
+        //     observacao.status = "Comum"
+        axios.post('http://localhost:10000/eventos', {
+            tipo: "ObservacaoClassificada",
+            dados: observacao
+        })
+    }
+}
+app.post('/eventos', (req, res) => {
+    funcoes[req.body.tipo](req.body.dados)
+    res.status(200).send({msg: 'ok'})
+})
+
+app.listen(7000, () => console.log ("Classificação. Porta 7000."))
